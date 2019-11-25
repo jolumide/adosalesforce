980fad2cadc2b663d9117bb42506007b6279e83b
jolumide <47488324+jolumide@users.noreply.github.com>
2019-11-25 23:22:53 +0000

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -2,6 +2,7 @@ import com.microsoft.demo.Demo;
 import org.junit.Test;
 //this is a test
 //This is test 2
+//This is my third test
 public class MyTest {
     @Test
     public void test_method_1() {
