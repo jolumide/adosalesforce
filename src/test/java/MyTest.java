3b411755fa9b05964576b33ed63a57400cba1067
jolumide <47488324+jolumide@users.noreply.github.com>
2019-11-26 10:03:03 +0000

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 //This is my third test
 //This is my 4th test
 // another one
-//Making a pull request
+//Making a pull request 
+//Making a pull request 2
 public class MyTest {
     @Test
     public void test_method_1() {
