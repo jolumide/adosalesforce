72bf97985a1d5b5b0037bff96e592e0621f1bc48
Olumide Akinwande <jolumideakinwande@yahoo.com>
2020-09-09 13:27:28 +0100

    modification

--- Stepscreaterecordsalesforce.py
+++ Stepscreaterecordsalesforce.py
@@ -7,10 +7,10 @@ from datetime import datetime
 
 #Arguments
 
-username='olumide.akinwande@bp.com.community.gptdev'
-password='0lum1dE£4%2014'
-client_id="3MVG95AcBeaB55lVC4ph4E3buPW7Texcr7qDeNnXA47bwtTa3W0hIKb5dZf9CCnnZuvkmjQKbOYBzExCAre0N"
-client_secret="E0D79A342987B8F876EB8E7BE3A889AC49726C006E09646BF031D1DA00C5666D"
+username=argv[1]
+password=argv[2]
+client_id=argv[3]
+client_secret=argv[4]
 #project_name=argv[5]
 #requested_date=argv[6] #format dd-mm-yy
 #release_rectyp=arg[7]
